f4b74a9d139febdef4615f746c139b9e4d972282
Vini <ixxvinicius@gmail.com>
2023-03-02 10:43:48 -0300

    Adicionando a propriedade courses

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,10 +1,13 @@
 
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Course } from './course';
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
+
+    courses: Course[] = [];
 
 }
